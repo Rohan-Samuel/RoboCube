b7bbce64be4e9510ddfe239eba8a5e3f8ceaa72b
RayOneThousand <rohan2309@gmail.com>
2025-11-20 15:24:32 -0800

    Fixed Head and Turret rotation

--- Assets/Scripts/Character/Player/PlayerAnimatorManager.cs
+++ Assets/Scripts/Character/Player/PlayerAnimatorManager.cs
@@ -1,15 +1,33 @@
+
 using UnityEngine;
+using UnityEngine.Animations.Rigging;
+
 
 public class PlayerAnimatorManager : CharacterAnimatorManager
 {
     PlayerManager player;
+    MultiAimConstraint headAim;
+
+    public float headRotationSpeed = 1f;
 
     protected override void Awake()
     {
         base.Awake();
 
         player = GetComponent<PlayerManager>();
+       
     }
+
+    private void Start()
+    {
+        headAim = PlayerManager.instance.GetComponentInChildren<MultiAimConstraint>();
+    }
+
+    private void Update()
+    {
+        RotationDamping();
+    }
+
     private void OnAnimatorMove()
     {
         if (player.applyRootMotion)
@@ -19,5 +37,37 @@ public class PlayerAnimatorManager : CharacterAnimatorManager
             player.transform.rotation *= player.animator.deltaRotation;
 
         }
+    }
+
+    private void RotationDamping()
+    {
+        Vector3 directionToTarget = headAim.data.sourceObjects[0].transform.position - player.transform.position;
+        float angleToTarget = Vector3.Angle(player.transform.forward, directionToTarget);
+
+
+        Rig rig = headAim.GetComponentInParent<Rig>();
+        Debug.Log("Angle to Target: " + angleToTarget);
+
+
+        Debug.Log(rig.weight);
+
+        if (angleToTarget > 140f)
+        {
+            rig.weight = Mathf.Lerp(rig.weight, 0, Time.deltaTime * headRotationSpeed);
+            
+        }
+        else if (angleToTarget > 110f)
+        {
+            rig.weight = Mathf.Lerp(rig.weight, .7f, Time.deltaTime * headRotationSpeed);
+
+        }
+        else
+        {
+            rig.weight = Mathf.Lerp(rig.weight, 1, Time.deltaTime * headRotationSpeed);
+        }
+
+
+
+
     }
 }
